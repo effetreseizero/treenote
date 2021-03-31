cfe9f07cbe6b7d768b61ca98ec68c6548e7d3256
Giacomo Colle <giacomo.colle@f360.it>
2021-03-31 16:20:27 +0200

    archived list

--- src/app/pages/surveys-manager/surveys-manager.page.ts
+++ src/app/pages/surveys-manager/surveys-manager.page.ts
@@ -18,6 +18,7 @@ export class SurveysManagerPage implements OnInit {
   sentSurveyList = [];
   reviewSurveyList = [];
   publicSurveyList = [];
+  archivedSurveyList = [];
 
   constructor(
     private surveysService: SurveysService,
@@ -60,7 +61,7 @@ export class SurveysManagerPage implements OnInit {
       this.sentSurveyList = this.surveysList.filter(x => (x.status === "sent"));
       this.reviewSurveyList = this.surveysList.filter(x => (x.status === "review"));
       this.publicSurveyList = this.surveysList.filter(x => (x.status === "public"));
-      this.publicSurveyList = this.surveysList.filter(x => (x.status === "archived"));
+      this.archivedSurveyList = this.surveysList.filter(x => (x.status === "archived"));
 
 
     });
