c0a877477f69dfbe415ad8ce4a6e48e066f8da22
giacomo.colle <giacomo.colle@crea.gov.it>
2024-09-30 09:03:05 +0200

    export button and service

--- src/app/services/export-data/export-data.service.ts
+++ src/app/services/export-data/export-data.service.ts
@@ -4,48 +4,70 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import * as XLSX from 'xlsx';
 
+import { FirebaseApp } from '@angular/fire/compat';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+
+import { CoreFacade } from "../storage/core.facade";
+import { User } from 'src/app/services/storage/user';
+
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+import { from, TimeoutError } from 'rxjs';
+
+import { compress, compressAccurately } from 'image-conversion';
+import { Firestore } from 'firebase/firestore';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class ExportDataService {
 
-  constructor() { }
+  collectionName = 'surveys';
+  data = null;
 
-  async exportSurveyToExcel(data) {
+  constructor(
+    private firestore: AngularFirestore,
+    private fireauth: AngularFireAuth,
+    private firestorage: AngularFireStorage,
+    private coreFacade: CoreFacade
+  ) { }
+
+  async exportSurveyToExcel(surveyID) {
+
+    this.firestore.doc(this.collectionName + '/' + surveyID).get().toPromise().then((document) => 
     {
+      this.data = document.data();
       const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(
         [{
-          accuratezza: 1,
-          alberi_morti:"",
-          avanzate:false,
-          commenti:"",
-          created_time:1727442264671,
-          data_ora_osservazione:"2024-09-27T13:02:34.724Z",
-          deleted:false,
-          diffusione_perc:"",
-          identificazione:"020_latifoglia",
-          latitudine:46.31097505201535,
-          loc_problema:"020_fusto",
-          localita:"test trento review",
-          longitudine:11.179353790020244,
-          modified_time:1727442303238,
-          nome_comune:"abete rosso",
-          nome_scientifico:"",
-          photo_0_imageurl:"https://firebasestorage.googleapis.com/v0/b/treenotepad.appspot.com/o/users_photo%2FASbl8a1FzLg8eN3lWfuKs1iKdJB2%2FtyDtxpVWo41KhP6Dv5tO%2F0.jpeg?alt=media&token=b5767a0e-2b52-4506-8fe6-294d4068bd82",
-          photo_0_storageuri:"users_photo/ASbl8a1FzLg8eN3lWfuKs1iKdJB2/tyDtxpVWo41KhP6Dv5tO/0.jpeg",
-          photo_1_imageurl:"",
-          photo_1_storageuri:"",
-          photo_2_imageurl:"",
-          photo_2_storageuri:"",
-          quota:0,
-          sintomo_0:"",
-          sintomo_1:"",
-          sintomo_2:"",
-          specie:"",
-          status:"archive",
-          tipologia:"010_gruppo",
-          user_email:"giacomo.colle@gmail.com",
-          user_uid:"ASbl8a1FzLg8eN3lWfuKs1iKdJB2",
+          accuratezza: this.data.accuratezza,
+          //alberi_morti: data.alberi_morti,
+          //avanzate:false,
+          commenti: this.data.commenti,
+          created_time: this.data.created_time,
+          data_ora_osservazione: this.data.data_ora_osservazione,
+          //diffusione_perc:"",
+          identificazione:this.data.identificazione,
+          latitudine: this.data.latitudine,
+          loc_problema: this.data.loc_problema,
+          localita: this.data.localita,
+          longitudine: this.data.longitudine,
+          modified_time: this.data.modified_time,
+          nome_comune: this.data.nome_comune,
+          //nome_scientifico:"",
+          photo_0_imageurl: this.data.photo_0_imageurl,
+          photo_1_imageurl: this.data.photo_1_imageurl,
+          photo_2_imageurl: this.data.photo_2_imageurl,
+          quota: this.data.quota,
+          //sintomo_0:"",
+          //sintomo_1:"",
+          //sintomo_2:"",
+          //specie:"",
+          status: this.data.status,
+          tipologia: this.data.tipologia,
+          
         }]
       );
       /*
@@ -67,6 +89,6 @@ export class ExportDataService {
       //XLSX.utils.book_append_sheet(wb, wm, "Models");
       //XLSX.utils.book_append_sheet(wb, wt, "Tree List");
       XLSX.writeFile(wb, "silvacuore_survey.xlsx");
-    }
+    });
   }
 }
